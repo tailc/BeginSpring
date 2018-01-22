d87bba41aac0a6e422bae2b67055684e807e09ee
tailc <tai.le@hubcba.com>
2018-01-22 18:15:13 +0700

    First-commit

--- ch02/src/main/java/com/congtai/ch02/Ch2App.java
+++ ch02/src/main/java/com/congtai/ch02/Ch2App.java
@@ -2,7 +2,6 @@ package com.congtai.ch02;
 
 import com.congtai.ch02.config.Ch2Config;
 import com.congtai.ch02.domain.Foo;
-import com.congtai.ch02.enviroment.Ch2Configuration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
@@ -13,13 +12,6 @@ import java.util.Collections;
 public class Ch2App {
 
     public static void main(String[] args) {
-//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Ch2Config.class);
-//        AccountService accountService = ctx.getBean(AccountService.class);
-//
-//        accountService.transferMoney(1, 2, 50);
-        //        MutablePropertySources propertySources = environment.getPropertySources();
-//        propertySources.addLast(new MapPropertySource("mapSource",
-//                Collections.singletonMap("name", "myfoo")));
 
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(Ch2Config.class);
@@ -27,11 +19,9 @@ public class Ch2App {
         ConfigurableEnvironment environment = ctx.getEnvironment();
         environment.setActiveProfiles("dev");
 
-
-
         MutablePropertySources propertySources = environment.getPropertySources();
         propertySources.addLast(new MapPropertySource("mapSource",
-                Collections.singletonMap("name", (Object)"my foo")));
+                Collections.singletonMap("name", (Object) "my foo")));
 
         ctx.refresh();
         Foo bean = ctx.getBean(Foo.class);
