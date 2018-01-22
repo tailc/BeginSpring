9080ed7cfe13feddce5d24944933c7ad2a2e5819
tailc <tai.le@hubcba.com>
2018-01-22 18:13:11 +0700

    First-commit

--- ch02/src/main/java/com/congtai/ch02/enviroment/Ch2Configuration.java
+++ ch02/src/main/java/com/congtai/ch02/enviroment/Ch2Configuration.java
@@ -8,25 +8,25 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 public class Ch2Configuration {
-	
-	@Bean
-	@Profile("dev")
-	public Foo devFoo(@Value("${name}") String name) {
-		Foo foo = new Foo();
-		foo.setName("dev " + name);
-		return foo;
-	}
-	
-	@Bean
-	@Profile("prod")
-	public Foo prodFoo(@Value("${name}") String name) {
-		Foo foo = new Foo();
-		foo.setName("prod " + name);
-		return foo;
-	}
-	
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
+
+    @Bean
+    @Profile("dev")
+    public Foo devFoo(@Value("${name}") String name) {
+        Foo foo = new Foo();
+        foo.setName("dev " + name);
+        return foo;
+    }
+
+    @Bean
+    @Profile("prod")
+    public Foo prodFoo(@Value("${name}") String name) {
+        Foo foo = new Foo();
+        foo.setName("prod " + name);
+        return foo;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
